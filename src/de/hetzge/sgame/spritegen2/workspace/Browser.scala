ddbbdd5dc1322f7b4d73a3d5c1dc0c8797699908
hetzge <hetzge@gmx.de>
2016-02-07 21:10:48 +0100

    Implemented first drag drop functionality

--- src/de/hetzge/sgame/spritegen2/workspace/Browser.scala
+++ src/de/hetzge/sgame/spritegen2/workspace/Browser.scala
@@ -28,6 +28,15 @@ import javafx.scene.control.ContentDisplay
 import javafx.geometry.Pos
 import javafx.scene.control.Accordion
 import javafx.scene.control.TitledPane
+import javafx.scene.input.MouseEvent
+import javafx.scene.control.ToolBar
+import javafx.scene.input.TransferMode
+import javafx.scene.input.DragEvent
+import javafx.scene.Cursor
+import javafx.scene.input.ClipboardContent
+import javafx.scene.input.DataFormat
+import javafx.scene.Node
+import javafx.collections.ObservableList
 
 object BrowserMain extends App {
   Application.launch(classOf[BrowserGuiApp], args: _*)
@@ -45,6 +54,10 @@ class BrowserGuiApp extends Application {
   }
 }
 
+object DragAction extends Enumeration {
+  val PLACE_BEFORE, PLACE_AFTER, REPLACE = Value
+}
+
 case class Animation(val name: String, val parts: java.util.ArrayList[Part]) {
   val partsProperty = FXCollections.observableList(parts)
 }
@@ -56,14 +69,17 @@ class Browser {
   val image1 = new Image(new FileInputStream(new File("/home/hetzge/private/werke/animation/test/b1.png")))
   val image2 = new Image(new FileInputStream(new File("/home/hetzge/private/werke/animation/test/e2.png")))
 
-  Model.Property.partsPool.add(Part("Part A", MutableList(image1)))
+  Model.Property.partsPool.add(Part("Part A", MutableList(image1, image2)))
   Model.Property.partsPool.add(Part("Part B", MutableList(image2)))
 
   val partsList = new java.util.ArrayList[Part]()
-  partsList.add(Part("Part A", MutableList(image1)))
+  partsList.add(Part("Part A", MutableList(image1, image2)))
   partsList.add(Part("Part B", MutableList(image2)))
 
   Model.Property.animationPool.add(Animation("Animation 1", partsList))
+  Model.Property.animationPool.add(Animation("Animation 2", partsList))
+  Model.Property.animationPool.add(Animation("Animation 3", partsList))
+  Model.Property.animationPool.add(Animation("Animation 4", partsList))
 
   object Model {
     val partsPool = new java.util.ArrayList[Part]()
@@ -120,11 +136,8 @@ class Browser {
                 super.updateItem(animation, empty)
                 setContentDisplay(ContentDisplay.GRAPHIC_ONLY)
                 if (animation != null) {
-                  object CellGraphic extends TitledPane(animation.name, CellGraphicBody)
-                  object CellGraphicBody extends Pane {
-
-                    getChildren().add(new Label("test"))
-                  }
+                  object CellGraphic extends TitledPane(animation.name, AnimationPartList)
+                  object AnimationPartList extends PartList(animation.partsProperty)
                   setGraphic(CellGraphic)
                 }
               }
@@ -141,41 +154,134 @@ class Browser {
 
     object PartBrowser extends VBox {
       Service.filterParts(PartFilter(null))
-      getChildren().add(PartList)
+      getChildren().add(new PartList(Model.Property.filteredParts))
       FxHelper.setAnchor(PartBrowser.this)
+    }
 
-      object PartList extends ListView[Part] {
-        setItems(Model.Property.filteredParts)
-        setCellFactory(PartCellFactory)
+    class PartList(val parts: ObservableList[Part]) extends ListView[Part] with PartCellHolder {
+      setItems(parts)
+      setCellFactory(PartCellFactory)
 
-        object PartCellFactory extends Callback[ListView[Part], ListCell[Part]] {
-          override def call(p: ListView[Part]): ListCell[Part] = {
-            object Cell extends ListCell[Part] {
-              override def updateItem(part: Part, empty: Boolean) = {
-                super.updateItem(part, empty)
-                setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
-                if (part != null) {
-                  object CellGraphic extends HBox {
-                    val firstImage = part.images.get(0)
-                    getChildren().add(firstImage match {
-                      case Some(image) => new ImageView(image)
-                      case None => new Pane()
-                    })
-                    getChildren().add(CellLabel)
-
-                    object CellLabel extends Label(part.name) {
-                      setSpacing(10.0d)
-                      setTranslateY(5.0d)
-                    }
-                  }
-                  setGraphic(CellGraphic)
-                }
+      object PartCellFactory extends Callback[ListView[Part], ListCell[Part]] {
+        override def call(p: ListView[Part]): ListCell[Part] = {
+          object Cell extends ListCell[Part] {
+            override def updateItem(part: Part, empty: Boolean) = {
+              super.updateItem(part, empty)
+              setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+              if (part != null) {
+                setGraphic(new PartCell(part, true))
               }
             }
-            Cell
           }
+          Cell
+        }
+      }
+
+      class PartCell(val part: Part, val dragGoal: Boolean = true) extends VBox with PartHolder {
+        getChildren().add(BeforeDrop)
+        getChildren().add(Content)
+        getChildren().add(AfterDrop)
+
+        trait DragGoal extends Node {
+          val action: DragAction.Value
+
+          DragGoal.this.setOnDragOver((dragEvent: DragEvent) => {
+            println("drag over")
+
+            dragEvent.acceptTransferModes(TransferMode.ANY: _*)
+            dragEvent.consume()
+          })
+
+          DragGoal.this.setOnDragDropped((dragEvent: DragEvent) => {
+
+            dragEvent.getGestureSource() match {
+              case partHolder: PartHolder => add(part, action, partHolder.part)
+              case _ => throw new IllegalStateException()
+            }
+
+            dragEvent.setDropCompleted(true)
+            dragEvent.consume()
+          })
+
+        }
+
+        object BeforeDrop extends ToolBar with DragGoal {
+          val action = DragAction.PLACE_BEFORE
+          setVisible(dragGoal)
+        }
+        object AfterDrop extends ToolBar with DragGoal {
+          val action = DragAction.PLACE_AFTER
+          setVisible(dragGoal)
+        }
+
+        object Content extends HBox with PartHolder with DragGoal {
+          val action = DragAction.REPLACE
+          val part = PartCell.this.part
+          val imageCells = part.images.map(new ImageCell(_))
+          getChildren().add(CellLabel)
+          getChildren().addAll(imageCells)
+
+          object CellLabel extends Label {
+            setText(part.name)
+            setSpacing(10.0d)
+            setTranslateY(5.0d)
+          }
+
+          Content.this.setOnDragDetected((mouseEvent: MouseEvent) => {
+            println("start drag")
+            val dragBoard = Content.this.startDragAndDrop(TransferMode.ANY: _*)
+            val content = new ClipboardContent()
+            content.putString("")
+            dragBoard.setContent(content);
+            mouseEvent.consume()
+          })
         }
       }
     }
+
+    trait PartHolder {
+      val part: Part
+    }
+
+    trait PartCellHolder {
+      val parts: ObservableList[Part]
+
+      def add(where: Part, action: DragAction.Value, part: Part): Unit = {
+        val index = parts.indexOf(where)
+        add(index, action, part)
+      }
+
+      def add(where: Int, action: DragAction.Value, part: Part): Unit = {
+        val offset = action match {
+          case DragAction.PLACE_AFTER => 1
+          case DragAction.PLACE_BEFORE => 0
+          case DragAction.REPLACE => 0
+          case _ => throw new IllegalStateException()
+        }
+
+        val index = if (where + offset < 0) {
+          0
+        } else if (where + offset >= parts.size()) {
+          parts.size() - 1
+        } else {
+          where + offset
+        }
+
+        println(where, index, part.name, action)
+
+        if (action == DragAction.REPLACE) {
+          parts.set(index, part)
+        } else {
+          parts.add(index, part)
+        }
+      }
+    }
+
+    class AnimationCell extends HBox {
+
+    }
+
+    class ImageCell(val image: Image) extends ImageView(image)
+
   }
 }
